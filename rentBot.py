2dad2a5e9885760364ae54ed10144bde8b909d44
Andrew L. Hill <AndrewLawrenceHill@gmail.com>
2014-03-24 13:51:20 -0700

    Newest, with fixed chart labels.

--- rentBot.py
+++ rentBot.py
@@ -8,7 +8,7 @@ import time
 
 anchor = "675995779"
 dataNew = []
-description = {"date": ("number", "Date"), "price0": ("number", "Rent"), "price1": ("number", "Rent"), "price2": ("number", "Rent"), "price3": ("number", "Rent")}
+description = {"date": ("number", "Date"), "price0": ("number", "0BR Rent"), "price1": ("number", "1BR Rent"), "price2": ("number", "2BR Rent"), "price3": ("number", "3BR Rent")}
 url1 = "http://polling.3taps.com/poll?auth_token=267b3ec711e58733c1fc2227ca30e555&anchor="
 url2 = "&category=RHFR&location.city=USA-SFO-ATH|USA-SFO-CUP|USA-SFO-LOA|USA-SFO-MEN|USA-SFO-MUA|USA-SFO-PAL|USA-SFO-RED|USA-SFO-STA|USA-SFO-SUN&retvals=timestamp,price,annotations,location"
 

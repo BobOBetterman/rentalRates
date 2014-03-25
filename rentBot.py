95869d7ac71daa663c9cc9aca3edca8484e627e0
Andrew L. Hill <AndrewLawrenceHill@gmail.com>
2014-03-24 21:00:15 -0700

    Now it sings and dances, as well.

--- rentBot.py
+++ rentBot.py
@@ -5,11 +5,12 @@ import urllib2
 import json
 import gviz_api
 import time
+import datetime
 
 #  If you get a key error on the anchor = data['anchor'] line below, it's probably because the initial anchor value was too low, and 3taps has already deleted it. Just increase the initial anchor value until the program runs.
 anchor = "617100000"
 #dataNew = []
-description = {"date": ("number", "Date"), "price0": ("number", "0BR Rent"), "price1": ("number", "1BR Rent"), "price2": ("number", "2BR Rent"), "price3": ("number", "3BR Rent")}
+description = {"date": ("datetime", "Date"), "price0": ("number", "0BR Rent"), "price1": ("number", "1BR Rent"), "price2": ("number", "2BR Rent"), "price3": ("number", "3BR Rent")}
 url1 = "http://polling.3taps.com/poll?auth_token=267b3ec711e58733c1fc2227ca30e555&anchor="
 url2 = "&category=RHFR&location.city=USA-SFO-ATH|USA-SFO-CUP|USA-SFO-LOA|USA-SFO-MEN|USA-SFO-MUA|USA-SFO-PAL|USA-SFO-RED|USA-SFO-STA|USA-SFO-SUN&retvals=timestamp,price,annotations,location"
 x = 0
@@ -26,11 +27,26 @@ while True:
 #  The next couple lines are for appending the data to the end of the existing file, rather than keeping the whole array in memory for ever. I think I'd like to do this instead, but I have to check to see how Google Charts handles multiple JSON objects first.
 #	f2 = open('chartData', 'a')
 	dataNew = []
+        dataOld = []
 
         if x > 0:
           f3 = open('chartDataRaw', 'r')
-          dataNew = json.load(f3)
+          dataOld = json.load(f3)
           f3.close()
+
+          j = 0
+
+          for item in dataOld:
+            if 'price0' in dataOld[j]:
+              dataNew.append({"date": datetime.datetime.utcfromtimestamp(dataOld[j]['date']), "price0": dataOld[j]['price0']})
+            if 'price1' in dataOld[j]:
+              dataNew.append({"date": datetime.datetime.utcfromtimestamp(dataOld[j]['date']), "price1": dataOld[j]['price1']})
+            if 'price2' in dataOld[j]:
+              dataNew.append({"date": datetime.datetime.utcfromtimestamp(dataOld[j]['date']), "price2": dataOld[j]['price2']})
+            if 'price3' in dataOld[j]:
+              dataNew.append({"date": datetime.datetime.utcfromtimestamp(dataOld[j]['date']), "price3": dataOld[j]['price3']})
+
+            j = j + 1
 
 	x = x + 1
 
@@ -50,22 +66,27 @@ while True:
 			if 'bedrooms' in data['postings'][i]['annotations']:
 
 				if data['postings'][i]['annotations']['bedrooms'] == 'studio':
-					dataNew.append({"date": data['postings'][i]['timestamp'], "price0": data['postings'][i]['price']})
+					dataNew.append({"date": datetime.datetime.utcfromtimestamp(data['postings'][i]['timestamp']), "price0": data['postings'][i]['price']})
+					dataOld.append({"date": data['postings'][i]['timestamp'], "price0": data['postings'][i]['price']})
 				elif data['postings'][i]['annotations']['bedrooms'] == '1br':
 #				dataNew.append({"date": data['postings'][i]['timestamp'], "price": data['postings'][i]['price'], "bedrooms": data['postings'][i]['annotations']['bedrooms'], "location": data['postings'][i]['location']['city']})
-					dataNew.append({"date": data['postings'][i]['timestamp'], "price1": data['postings'][i]['price']})
+#					dataNew.append({"date": datetime.datetime.utcfromtimestamp(data['postings'][i]['timestamp']).strftime('new Date(%Y, %m, %d, %H, %M, %S)'), "price1": data['postings'][i]['price']})
+					dataNew.append({"date": datetime.datetime.utcfromtimestamp(data['postings'][i]['timestamp']), "price1": data['postings'][i]['price']})
+					dataOld.append({"date": data['postings'][i]['timestamp'], "price1": data['postings'][i]['price']})
 				elif data['postings'][i]['annotations']['bedrooms'] == '2br':
 #				dataNew.append({"date": data['postings'][i]['timestamp'], "price": data['postings'][i]['price'], "bedrooms": data['postings'][i]['annotations']['bedrooms'], "location": data['postings'][i]['location']['city']})
-					dataNew.append({"date": data['postings'][i]['timestamp'], "price2": data['postings'][i]['price']})
+					dataNew.append({"date": datetime.datetime.utcfromtimestamp(data['postings'][i]['timestamp']), "price2": data['postings'][i]['price']})
+					dataOld.append({"date": data['postings'][i]['timestamp'], "price2": data['postings'][i]['price']})
 				elif data['postings'][i]['annotations']['bedrooms'] == '3br':
 #				dataNew.append({"date": data['postings'][i]['timestamp'], "price": data['postings'][i]['price'], "bedrooms": data['postings'][i]['annotations']['bedrooms'], "location": data['postings'][i]['location']['city']})
-					dataNew.append({"date": data['postings'][i]['timestamp'], "price3": data['postings'][i]['price']})
+					dataNew.append({"date": datetime.datetime.utcfromtimestamp(data['postings'][i]['timestamp']), "price3": data['postings'][i]['price']})
+					dataOld.append({"date": data['postings'][i]['timestamp'], "price3": data['postings'][i]['price']})
 
 
 
 		i = i + 1
 
-        json.dump(dataNew, f3)
+        json.dump(dataOld, f3)
         f3.close()
 
 	dataTable = gviz_api.DataTable(description)

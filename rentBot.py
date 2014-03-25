e83312506b318f6743921e08f114034a178687ea
Andrew L. Hill <AndrewLawrenceHill@gmail.com>
2014-03-24 18:45:32 -0700

    Okay, now the bot is actually on

--- rentBot.py
+++ rentBot.py
@@ -6,15 +6,16 @@ import json
 import gviz_api
 import time
 
-anchor = "616100000"
+#  If you get a key error on the anchor = data['anchor'] line below, it's probably because the initial anchor value was too low, and 3taps has already deleted it. Just increase the initial anchor value until the program runs.
+anchor = "617100000"
 #dataNew = []
 description = {"date": ("number", "Date"), "price0": ("number", "0BR Rent"), "price1": ("number", "1BR Rent"), "price2": ("number", "2BR Rent"), "price3": ("number", "3BR Rent")}
 url1 = "http://polling.3taps.com/poll?auth_token=267b3ec711e58733c1fc2227ca30e555&anchor="
 url2 = "&category=RHFR&location.city=USA-SFO-ATH|USA-SFO-CUP|USA-SFO-LOA|USA-SFO-MEN|USA-SFO-MUA|USA-SFO-PAL|USA-SFO-RED|USA-SFO-STA|USA-SFO-SUN&retvals=timestamp,price,annotations,location"
 x = 0
 
-#while True:
-while x < 2:
+while True:
+#while x < 2:
 #	url = "http://polling.3taps.com/poll?auth_token=267b3ec711e58733c1fc2227ca30e555&anchor=675995779&category=RHFR&location.city=USA-SFO-ATH|USA-SFO-CUP|USA-SFO-LOA|USA-SFO-MEN|USA-SFO-MUA|USA-SFO-PAL|USA-SFO-RED|USA-SFO-STA|USA-SFO-SUN&retvals=timestamp,price,annotations,location"
 	url = url1 + anchor + url2
 	data = urllib2.urlopen(url).read()
@@ -78,8 +79,8 @@ while x < 2:
 	f2.close()
 
 #  Let's make the call every three hours:
-#	time.sleep(10800)
+	time.sleep(10800)
 
 #  I was making the call every eight hours originally, but I'd rather it catch up to present time faster, and I want to make very sure it keeps ahead of postings, rather than falling behind. It's not like calling eight times a day is going to break their system.
 #	time.sleep(28800)
-	time.sleep(30)
+#	time.sleep(30)
